baff74a90ec698d61e72915c10ab003b1ed76e5b
Bohdan Vanchuhov <bodya.van@gmail.com>
2017-07-01 20:56:11 +0300

    add shop example

---
+++ src/com/bvan/javaoop/sample/oop/shop/ShopRunner.java
@@ -0,0 +1,19 @@
+package com.bvan.javaoop.sample.oop.shop;
+
+/**
+ * @author bvanchuhov
+ */
+public class ShopRunner {
+
+    public static void main(String[] args) {
+        User user = new User("John", new Account(12_000));
+
+        user.addProduct(new Product("MacBook", 2000), 2);
+        user.addProduct(new Product("Lenovo", 1500), 3);
+
+        user.payForCurrentOrder();
+
+        System.out.println("remainingMoney = " + user.getRemainingMoney()); // 3500
+        System.out.println("completedOrders = " + user.getCompletedOrders());
+    }
+}
